3bf4273f9d1996f12bbf35023f1e318c5c4923d0
hugoxxxxx <52855225+hugoxxxxx@users.noreply.github.com>
2024-06-13 11:38:46 +0800

    Update mao.js

--- mao.js
+++ mao.js
@@ -359,55 +359,195 @@ function getSessionId() {
 
 
 // 获取门店库存
-function getShopInfo() {
-  console.log(`获取[${MT_PROVINCE + MT_CITY + MT_DISTRICT}]门店库存:\n`);
-  let opt = {
-    url: `https://static.moutai519.com.cn/mt-backend/xhr/front/mall/shop/list/slim/v3/${$.sessionId}/${encodeURIComponent(MT_PROVINCE)}/10056/${zeroDate}`,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    }
-  }
-  // console.log(opt);
-  return new Promise(resolve => {
-    $.get(opt, (err, resp, data) => {
-      try {
-        if (err) $.log(err);
-        if (data) {
-          data = JSON.parse(data);
-          if (data?.code === 2000) {
-            productInfo = data.data.items;
-            shopInfo = data.data.shops;
-            let shops = data.data.shops;
-            $.stock = {}
-            for (const _ in shops) {
-              const shopId = shops[_].shopId;
-              const items = shops[_].items;
-              if ($.shopMap[shopId]['cityName'] === MT_CITY) {
-                if (MT_DISTRICT && !new RegExp(MT_DISTRICT).test($.shopMap[shopId]['districtName'])) continue;
-                for (const _ in items) {
-                  const { count, itemId, inventory, ownerName } = items[_];
-                  if (!$.stock[itemId]) $.stock[itemId] = [];
-                  $.stock[itemId].push(shopId);
-                  console.log(`【${ownerName}】[${shopId}-${itemId}] ${getProductInfo(itemId, 'title')}  价格:${getProductInfo(itemId, 'price')}  库存:${inventory}`);
-                  // console.log(`门店:${ownerName}  商品[${itemId}]:${getProductInfo(itemId, 'title')}  价格:${getProductInfo(itemId, 'price')}  库存:${inventory}`);
-                }
-              }
-            }
-            // console.log($.stock);
-          } else {
-            console.log(`查询失败`, data);
-          }
-        } else {
-          $.log("⚠️ 服务器返回了空数据\n");
-        }
-      } catch (error) {
-        $.log(error);
-      } finally {
-        resolve();
-      }
-    });
-  });
+function randomArr(arr) {
+    return arr[parseInt(Math.random() * arr.length, 10)];
 }
+
+function Env(t, e) {
+    "undefined" != typeof process && JSON.stringify(process.env).indexOf("GITHUB") > -1 && process.exit(0);
+    class s {
+        constructor(t) {
+            this.env = t;
+        }
+
+        send(t, e = "GET") {
+            t = "string" == typeof t ? { url: t } : t;
+            let s = this.get;
+            return "POST" === e && (s = this.post), new Promise((e, i) => {
+                s.call(this, t, (t, s, r) => {
+                    t ? i(t) : e(s);
+                });
+            });
+        }
+
+        get(t) {
+            return this.send.call(this.env, t);
+        }
+
+        post(t) {
+            return this.send.call(this.env, t, "POST");
+        }
+    }
+
+    return new class {
+        constructor(t, e) {
+            this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `🔔${this.name}, 开始!`);
+        }
+
+        isNode() {
+            return "undefined" != typeof module && !!module.exports;
+        }
+
+        isQuanX() {
+            return "undefined" != typeof $task;
+        }
+
+        isSurge() {
+            return "undefined" != typeof $httpClient && "undefined" == typeof $loon;
+        }
+
+        isLoon() {
+            return "undefined" != typeof $loon;
+        }
+
+        toObj(t, e = null) {
+            try {
+                return JSON.parse(t);
+            } catch {
+                return e;
+            }
+        }
+
+        toStr(t, e = null) {
+            try {
+                return JSON.stringify(t);
+            } catch {
+                return e;
+            }
+        }
+
+        getjson(t, e) {
+            let s = e;
+            const i = this.getdata(t);
+            if (i) try {
+                s = JSON.parse(this.getdata(t));
+            } catch { }
+            return s;
+        }
+
+        setjson(t, e) {
+            try {
+                return this.setdata(JSON.stringify(t), e);
+            } catch {
+                return !1;
+            }
+        }
+
+        getScript(t) {
+            return new Promise(e => {
+                this.get({ url: t }, (t, s, i) => e(i));
+            });
+        }
+
+        runScript(t, e) {
+            return new Promise(s => {
+                let i = this.getdata("@chavy_boxjs_userCfgs.httpapi");
+                i = i ? i.replace(/\n/g, "").trim() : i;
+                let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");
+                r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r;
+                const [o, h] = i.split("@"), n = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, accept: "*/*" } };
+                this.post(n, (t, e, i) => s(i));
+            }).catch(t => this.logErr(t));
+        }
+
+        loaddata() {
+            if (!this.isNode()) return {};
+            {
+                this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path");
+                const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e);
+                if (!s && !i) return {};
+                {
+                    const i = s ? t : e;
+                    try {
+                        return JSON.parse(this.fs.readFileSync(i));
+                    } catch (t) {
+                        return {};
+                    }
+                }
+            }
+        }
+
+        writedata() {
+            if (this.isNode()) {
+                this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path");
+                const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data);
+                s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r);
+            }
+        }
+
+        lodash_get(t, e, s) {
+            const i = e.replace(/\[(\d+)\]/g, ".$1").split(".");
+            let r = t;
+            for (const t of i)
+                if (r = Object(r)[t], void 0 === r) return s;
+            return r;
+        }
+
+        lodash_set(t, e, s) {
+            return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t);
+        }
+
+        getdata(t) {
+            let e = this.getval(t);
+            if (/^@/.test(t)) {
+                const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : "";
+                if (r) try {
+                    const t = JSON.parse(r);
+                    e = t ? this.lodash_get(t, i, "") : e;
+                } catch (t) {
+                    e = "";
+                }
+            }
+            return e;
+        }
+
+        setdata(t, e) {
+            let s = !1;
+            if (/^@/.test(e)) {
+                const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}";
+                try {
+                    const e = JSON.parse(h);
+                    this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i);
+                } catch (e) {
+                    const o = {};
+                    this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i);
+                }
+            } else s = this.setval(t, e);
+            return s;
+        }
+
+        getval(t) {
+            return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null;
+        }
+
+        setval(t, e) {
+            return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null;
+        }
+
+        initGotEnv(t) {
+            this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar));
+        }
+
+        get(t, e = (() => { })) {
+            t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => {
+                !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i);
+            })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => {
+                const { statusCode: s, statusCode: i, headers: r, body: o } = t;
+                e(null, { status: s, statusCode: i, headers: r, body: o }, o);
+            }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { }));
+        }
+
+        post(t, e = (()
 
 
 // 获取actParam
